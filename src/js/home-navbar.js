ec12906beedf53dbc5b723234d348f619e9adbb3
vinicius inocêncio <vinicius1silva61@gmail.com>
2025-07-23 10:27:01 -0300

    criacao-botao-hamburguer

---
+++ src/js/home-navbar.js
@@ -0,0 +1,9 @@
+const mobileMenu = document.querySelector(".mobile-menu");
+const navList = document.querySelector(".nav-list");
+
+mobileMenu.addEventListener("click", ativarMenu);
+
+
+function ativarMenu(){
+    navList.classList.toggle("active")
+}
